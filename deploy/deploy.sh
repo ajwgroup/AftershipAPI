281c2c4b4ee468117be60169c2241c7e6530fa99
root <root@LAPTOP013.europe.ajw>
2018-12-18 13:36:29 +0000

    update the deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,6 @@ ApiKey=$1
 ts=$(date +"%y%m%d%H%M")
 
 echo "Starting pack"
-dotnet pack AMOS/AMOS.csproj /p:PackageVersion=1.0.$ts --configuration Release
+dotnet pack AftershipAPI/AftershipAPI.csproj /p:PackageVersion=1.0.$ts --configuration Release
 echo "Starting push"
-dotnet nuget push AMOS/bin/Release/*1.0.$ts.nupkg --source https://api.nuget.org/v3/index.json --api-key $ApiKey
+dotnet nuget push AftershipAPI/bin/Release/*1.0.$ts.nupkg --source https://api.nuget.org/v3/index.json --api-key $ApiKey
