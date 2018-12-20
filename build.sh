f884b40e95a2d1ce34923c02aac067774e682c0f
Adam Worley <adam.worley@ajw-group.com>
2018-12-20 11:02:46 +0000

    use cake to install dotnet in script folder

--- build.sh
+++ build.sh
@@ -20,16 +20,16 @@ fi
 # INSTALL .NET CORE CLI
 ###########################################################################
 
-# echo "Installing .NET CLI..."
-# if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
-#   mkdir "$SCRIPT_DIR/.dotnet"
-# fi
-# curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh
-# sudo bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version 1.0.0-preview3-003223 --install-dir .dotnet --no-path
-# export PATH="$SCRIPT_DIR/.dotnet":$PATH
-# export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-# export DOTNET_CLI_TELEMETRY_OPTOUT=1
-# "$SCRIPT_DIR/.dotnet/dotnet" --info
+echo "Installing .NET CLI..."
+if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
+  mkdir "$SCRIPT_DIR/.dotnet"
+fi
+curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh
+sudo bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version 2.2.0 --install-dir .dotnet --no-path
+export PATH="$SCRIPT_DIR/.dotnet":$PATH
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+"$SCRIPT_DIR/.dotnet/dotnet" --info
 
 
 ###########################################################################
