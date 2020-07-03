deba79f963bff98500fb89b24531e8d7f632d3d9
Adam Worley <Adam.Worley@ajw-group.com>
2020-07-03 16:50:17 +0100

    Update tests to reflect changes and increase code coverage

---
+++ tests/AftershipAPITests/ConnectionApiTests.cs
@@ -0,0 +1,83 @@
+﻿using AftershipAPI;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Newtonsoft.Json;
+using System;
+
+namespace AftershipAPITests
+{
+    [TestClass]
+    public class ConnectionApiTests
+    {
+        [TestMethod]
+        public void NewConnectionAPI_OnlyApiKeyArgument_NullApiKeyString_ThrowsArgumentError()
+        {
+            Action act = () => new ConnectionAPI(null);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void NewConnectionAPI_OnlyApiKeyArgument_WhiteSpaceApiKeyString_ThrowsArgumentError()
+        {
+            Action act = () => new ConnectionAPI(" ");
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void NewConnectionAPI_OnlyApiKeyArgument_ApiKeyString_CreatesConnectionApi()
+        {
+            var result = new ConnectionAPI("key");
+
+            result.Should().NotBeNull();
+        }
+
+        [DataTestMethod]
+        [DataRow(null, null)]
+        [DataRow(null, " ")]
+        [DataRow(" ", null)]
+        [DataRow(" ", " ")]
+        public void NewConnectionAPI_KeyAndUrlArguments_EmptyString_ThrowsArgumentError(string? key, string? url)
+        {
+            Action act = () => new ConnectionAPI(key, url);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void NewConnectionAPI_ApiAndUrlArgument_ApiKeyString_CreatesConnectionApi()
+        {
+            var result = new ConnectionAPI("key", "url");
+
+            result.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void CreateTracking()
+        {
+            var requestClient = new Mock<IRequestClient>();
+
+            var tracking = new Tracking("tracking");
+
+            var response = new Response()
+            {
+                Meta = new Meta() { Code = 200 },
+                Data = new Data()
+                {
+                    Tracking = tracking
+                }
+            };
+
+            string responseString = JsonConvert.SerializeObject(response);
+            requestClient.Setup(x => x.RunRequest(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(responseString);
+
+            var connectionApi = new ConnectionAPI("", "", requestClient.Object);            
+
+            var result = connectionApi.CreateTracking(tracking);
+
+            requestClient.Verify(x => x.RunRequest("POST", It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+    }
+}
--- tests/AftershipAPITests/CourierTests.cs
+++ tests/AftershipAPITests/CourierTests.cs
@@ -1,6 +1,8 @@
 ﻿using AftershipAPI;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace AftershipAPITests
 {
@@ -15,6 +17,25 @@ namespace AftershipAPITests
             var result = courier.ToString();
 
             result.Should().Be("Courier{slug='slug', name='name', phone='phone', other_name='other_name', web_url='web'}");
+        }
+
+        [TestMethod]
+        public void Courier()
+        {
+            JObject o = new JObject(
+               new JProperty("slug", "slug"),
+               new JProperty("name", "name"),
+               new JProperty("phone", "phone"),
+               new JProperty("other_name", "other_name"),
+               new JProperty("web_url", "web_url"),
+               new JProperty("required_fields", new List<string> { "required_fields" })
+            );
+
+            var result = o.ToObject<Courier>();
+
+            var expected = new Courier("web_url", "slug", "name", "phone", "other_name");
+
+            result.Should().BeEquivalentTo(expected);
         }
     }
 }
